2c577b8b46851ef4cc80a3df2dc54e0ca81662ef
JKilkenny813 <julian_kilkenny@yahoo.com>
2021-04-07 21:42:07 -0600

    Update page-2.js

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -7,8 +7,26 @@ import SEO from "../components/seo"
 const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <h1>Labber Secuirty Blog </h1>
+    <p>Welcome to the Labber Secuirty Blog 
+  <break>
+  4/8/2021 J.Kilkenny
+  
+  In this blog we are to going to attempt to answer a few questions about dapp use for decentralized or autonomous organizations.
+  
+  First off what is a dapp? A dapp is a decentralized application. 
+  
+1.	What are the biggest practical barriers to dapps adoption?
+  
+2.	What are the security advantages and disadvantages of dapps?
+  
+3.	How might the role of a system administrator change in an organization that made use of dapps?
+  
+4.	Is it possible to manage dapps for a team of users like we might with traditional SaaS business applications? If so, how might it be done? If not, what are the implications for organizational dapp use?
+
+  
+  
+  /p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )

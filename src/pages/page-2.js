806372024ea1aaf3396541200231658b0a142943
JKilkenny813 <julian_kilkenny@yahoo.com>
2021-04-08 10:43:17 -0600

    Update page-2.js

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,66 +1,59 @@
- import React from "react" import { Link } from "gatsby"
+import React from "react"
+import {
+	Link
+} from "gatsby"
 
-import Layout from "../components/layout" import SEO from "../components/seo"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
 
-const SecondPage = () => ( <Layout> <SEO title="Page two" /> <h1>Welcome to the
-Labber Security Blog</h1> <p> <body>
-    
-    <p> J.Kilkenny
+const SecondPage = () => ( < Layout > < SEO title = "Page two" / > < h1 > Welcome to the Labber Security Blog < /h1> <p> <body>
 
-<p>  In this blog we are to going to attempt to answer a few questions about dApp
-  use for decentralized or autonomous organizations.</p>
+		<
+		p > J.Kilkenny
 
- <p> First off what is a dApp? A dApp is a decentralized application. </p>
+		<
+		p > In this blog we are to going to attempt to answer a few questions about dApp use
+		for decentralized or autonomous organizations. < /p>
 
-<p>What are the biggest practical barriers to dApps adoption?</p>
+		<
+		p > First off what is a dApp ? A dApp is a decentralized application. < /p>
 
-  <p>One of the biggest barriers to dApps adoption is lack of knowledge or
-  understanding around how easy and secure these applications are to create and
-  deploy. For example,   This blog was created in about 2 hours. There was no
-  background knowledge on this previously and therefore I utilized what I was
-  able to find from the resources available. </p>
+		<
+		p > What are the biggest practical barriers to dApps adoption ? < /p>
 
-<p>	What are the security advantages and disadvantages of dapps?</p> <p>Here are
-some of the advantages of utilizing a dApp; Simple to develop, more control,
-safe data, open source, transparent data, less downtime, data is never lost,
-can’t be blocked, cost reduction. Disadvantages are dAps infra is harder to
-maintain and develop, traditional security does not work, lack of user
-experience, slow speeds. Particularly speaking of traditional security dApps
-dont have a single point of failure which means more resistance to attacks,
-(every computer on the system would need to be taken down in order to cause a
-point of failure in the system. The negative to this is that traditional
-security solutions dont work and a dApp specific solution needs to be
-implemented. Valid Networks blockchain security platform for dApps does just
-that.
+		<
+		p > One of the biggest barriers to dApps adoption is lack of knowledge or understanding around how easy and secure these applications are to create and deploy.For example, This blog was created in about 2 hours.There was no background knowledge on this previously and therefore I utilized what I was able to find from the resources available. < /p>
 
-  </p>
+		<
+		p > What are the security advantages and disadvantages of dapps ? < /p> <p>Here are
+		some of the advantages of utilizing a dApp; Simple to develop, more control,
+		safe data, open source, transparent data, less downtime, data is never lost,
+		can’ t be blocked, cost reduction.Disadvantages are dAps infra is harder to maintain and develop, traditional security does not work, lack of user experience, slow speeds.Particularly speaking of traditional security dApps dont have a single point of failure which means more resistance to attacks,
+		(every computer on the system would need to be taken down in order to cause a point of failure in the system.The negative to this is that traditional security solutions dont work and a dApp specific solution needs to be implemented.Valid Networks blockchain security platform
+			for dApps does just that.
 
-	<p> How might the role of a system administrator change in an organization that
-	made use of dapps?</p>
+			<
+			/p>
 
-<p>  Some of the system administrator tasks may be off loaded by being automated
-so the role itself may become more diversified and look more like a system
-engineer.   The way that system admins handle security should also be impacted
-as they will no longer be able to utilize traditional methods of security. They
-can however enforce static separation of duties by configuring pre-requisite
-conditions. </p>
+			<
+			p > How might the role of a system administrator change in an organization that made use of dapps ? < /p>
+
+			<
+			p > Some of the system administrator tasks may be off loaded by being automated so the role itself may become more diversified and look more like a system engineer.The way that system admins handle security should also be impacted as they will no longer be able to utilize traditional methods of security.They can however enforce static separation of duties by configuring pre - requisite conditions. < /p>
 
 
-  
-<p>	Is it possible to manage dApps for a team of users like we might with
-traditional SaaS business applications? If so, how might it be done? If not,
-what are the implications for organizational dapp use?</p> <p> Dealing with
-browser plugins, extensions or entirely new and unfamiliar crypto-enabled
-browsers, along with the steep learning curve of private key security and backup
-phrases, is too high a barrier for the vast majority of users, especially those
-using a mobile device as the vast majority of users utilize mobile devices to
-access the internet. With that said it is possible to manage a dApp for a team
-of users but deciding on the way to approach the system account depends on the
-mission and goal of the users and the application. </p>
+			<
+			p > Is it possible to manage dApps
+			for a team of users like we might with traditional SaaS business applications ? If so, how might it be done ? If not,
+			what are the implications
+			for organizational dapp use ? < /p> <p> Dealing with
+			browser plugins, extensions or entirely new and unfamiliar crypto - enabled browsers, along with the steep learning curve of private key security and backup phrases, is too high a barrier
+			for the vast majority of users, especially those using a mobile device as the vast majority of users utilize mobile devices to access the internet.With that said it is possible to manage a dApp
+			for a team of users but deciding on the way to approach the system account depends on the mission and goal of the users and the application. < /p>
 
-    </p>
-</body>
-    <Link to="/">Go back to the homepage</Link>   </Layout> )
+			<
+			/p> <
+			/body> <
+			Link to = "/" > Go back to the homepage < /Link>   </Layout > )
 
-export default SecondPage © 2021 GitHub, Inc. Terms Privacy Security Status Docs
-Contact GitHub Pricing API Training Blog About Loading complete
+		export default SecondPage© 2021 GitHub, Inc.Terms Privacy Security Status Docs Contact GitHub Pricing API Training Blog About Loading complete
